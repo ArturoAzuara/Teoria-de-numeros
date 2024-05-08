e2cd8c56ff99764e63c560209ea9ed0ef9b2903c
Arturo Azuara <AAzuara@protalent-mx.com>
2024-05-08 10:29:21 -0600

    se añade clave pub y priv

--- main.py
+++ main.py
@@ -54,28 +54,57 @@ def mcd(producto_de_phi, num):
 def son_coprimos(producto_de_phi, num):
     return mcd(producto_de_phi, num) == 1
 
+def inverso_modular(a, m):
+    r0, r1 = m, a
+    x0, x1 = 0, 1
+    while r1 != 0:
+        q = r0 // r1
+        r0, r1 = r1, r0 - q * r1
+        x0, x1 = x1, x0 - q * x1
+    if r0 != 1:
+        return None
+    else:
+        return x0 % m
 
-try:
-    numero_p = int(input("Introduce 1er numero primo: "))
-    while not comp_1er_num_primo(numero_p):
+while True:
+    try:
         numero_p = int(input("Introduce 1er numero primo: "))
+        while not comp_1er_num_primo(numero_p):
+            numero_p = int(input("Introduce 1er numero primo: "))
 
-    numero_q = int(input("Introduce 2nd numero primo: "))
-    while not comp_2nd_num_primo(numero_q) or num_iguales(numero_p, numero_q):
         numero_q = int(input("Introduce 2nd numero primo: "))
-    print(f"Los numeros introducidos han sido, {numero_p} y {numero_q}.")
+        while not comp_2nd_num_primo(numero_q) or num_iguales(numero_p, numero_q):
+            numero_q = int(input("Introduce 2nd numero primo: "))
+        print("")
+        print(f"Los numeros introducidos han sido, {numero_p} y {numero_q}.")
+        print("")
 
-    producto_n = numero_p * numero_q
-    print(f"El resultado del producto de 'n' es: {producto_n}")
-    producto_de_phi = (numero_p - 1) * (numero_q - 1)
-    print(f"El resultado del producto de 'phi' es: {producto_de_phi}")
+        producto_n = numero_p * numero_q
+        print("")
+        print(f"El resultado del producto de 'n' es: {producto_n}")
+        print("")
+        producto_de_phi = (numero_p - 1) * (numero_q - 1)
+        print("")
+        print(f"El resultado del producto de 'phi' es: {producto_de_phi}")
+        print("")
 
-    calculo_mcd = int(input("Introduce un numero coprimo con phi: "))
-    while not son_coprimos(producto_de_phi, calculo_mcd):
-        print(f"{calculo_mcd} no es coprimo con {producto_de_phi}.")
-        calculo_mcd = int(input("Introduce un numero coprimo con phi: "))
-    print(f"MCD ({producto_de_phi}, {calculo_mcd})")
+        while True:
+            calculo_mcd = int(input("Introduce un numero coprimo con phi: "))
+            if not son_coprimos(producto_de_phi, calculo_mcd):
+                print(f"{calculo_mcd} no es coprimo con {producto_de_phi}.")
+            else:
+                print(f"{calculo_mcd} es coprimo con {producto_de_phi}.")
+                print(f"MCD ({producto_de_phi}, {calculo_mcd})")
+                inverso = inverso_modular(calculo_mcd, producto_de_phi)
+                print("")
+                print(f"El inverso modular de: ({calculo_mcd} y {producto_de_phi}) es = ", inverso)
+                print("")
+                break
+        print("")
+        print(f"Tu clave pública es: ({calculo_mcd},{producto_n})")
+        print(f"Tu clave privada es: ({inverso},{producto_n})")
+        print("")
+    except ValueError:
+        print("Error, intenta de nuevo")
+    break
 
-
-except ValueError:
-    print("Error, intenta de nuevo")
